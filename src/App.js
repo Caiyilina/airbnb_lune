b0a61edeadf6afa25db674c8a497ffe43d8cbc39
caiyilin@aiiiin.com <caiyilin@aiiiin.com>
2024-11-19 18:01:54 +0800

    导航栏初始化

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { memo } from "react";
 import { useRoutes } from "react-router-dom";
-import routes from "./router";
+import routes from "@/router";
+import AppHeader from "@/components/app-header";
+import AppFooter from "@/components/app-footer";
 
 const App = memo(() => {
   return (
     <div className="app">
-      <div className="header">Header</div>
+      <div className="header">
+        <AppHeader />
+      </div>
       <div className="page">{useRoutes(routes)}</div>
-      <div className="footer">Footer</div>
+      <div className="footer">
+        <AppFooter />
+      </div>
     </div>
   );
 });
