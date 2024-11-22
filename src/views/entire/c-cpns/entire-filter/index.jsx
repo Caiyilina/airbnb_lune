40e3f992c04a2ae605ed7ba88d16550f2c745709
caiyilin@aiiiin.com <caiyilin@aiiiin.com>
2024-11-22 18:01:04 +0800

    entire init

--- src/views/entire/c-cpns/entire-filter/index.jsx
+++ src/views/entire/c-cpns/entire-filter/index.jsx
@@ -1,9 +1,20 @@
 import PropTypes from "prop-types";
 import React, { memo } from "react";
 import { EntireFilterWrapper } from "./style";
+import filterData from "src/assets/data/filter_data.json";
 
 const EntireFilter = memo((props) => {
-  return <EntireFilterWrapper>EntireFilter</EntireFilterWrapper>;
+  return (
+    <EntireFilterWrapper>
+      {filterData.map((item, index) => {
+        return (
+          <div className="item" key={index}>
+            {item}
+          </div>
+        );
+      })}
+    </EntireFilterWrapper>
+  );
 });
 
 EntireFilter.propTypes = {};
