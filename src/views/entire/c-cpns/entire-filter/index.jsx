5666efc0cdaa22ff4bd8dfc975254f719e77264a
caiyilin@aiiiin.com <caiyilin@aiiiin.com>
2024-11-22 17:57:55 +0800

    初始化详情页布局

---
+++ src/views/entire/c-cpns/entire-filter/index.jsx
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+import React, { memo } from "react";
+import { EntireFilterWrapper } from "./style";
+
+const EntireFilter = memo((props) => {
+  return <EntireFilterWrapper>EntireFilter</EntireFilterWrapper>;
+});
+
+EntireFilter.propTypes = {};
+
+export default EntireFilter;
